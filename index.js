4397bb41042ced92f324b31321fa3b7eaa306f91
Crim, Austin <Crim.Austin@principal.com>
2019-09-18 13:36:15 -0500

    dark mode

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+$(function() {
+  $('[data-toggle="tooltip"]').tooltip();
 
-    $('#phone-link').on('focusin', function() {
-      $('[data-toggle="tooltip"]').tooltip('show');
-    });
-    
-    $('#phone-link').on('focusout', function() {
-      $('[data-toggle="tooltip"]').tooltip('hide');
-    });
-
-
+  $("#phone-link").on("focusin", function() {
+    $('[data-toggle="tooltip"]').tooltip("show");
   });
+
+  $("#phone-link").on("focusout", function() {
+    $('[data-toggle="tooltip"]').tooltip("hide");
+  });
+
+  $(".dark-mode-btn").on("click", function() {
+      $(".dark-mode-btn").toggleClass("dark-mode");
+      $(".white-section").toggleClass("dark-mode");
+
+    }
+  );
+});

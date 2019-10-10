e9f0ec68d4a085a5a81ffc59bb45353d151b19f7
Crim, Austin <Crim.Austin@principal.com>
2019-10-10 12:10:21 -0500

    cleaned up index.js file

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 $(function() {
-  $('[data-toggle="tooltip"]').tooltip();
-
-  $("#phone-link").on("focusin", function() {
-    $('[data-toggle="tooltip"]').tooltip("show");
-  });
-
-  $("#phone-link").on("focusout", function() {
-    $('[data-toggle="tooltip"]').tooltip("hide");
-  });
-
   $(".dark-mode-btn").on("click", function() {
       $("*").toggleClass("dark-mode");
     }

b630d1ac6731ff682a1ce2784e2e12adc2054f3f
Crim, Austin <Crim.Austin@principal.com>
2019-10-14 12:29:22 -0500

    fixed navbar, smooth scroll

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 $(function() {
   $(".dark-mode-btn").on("click", function() {
-      $("*").toggleClass("dark-mode");
-    }
-  );
+    $("*").toggleClass("dark-mode");
+  });
+  
+  $(document).on("click", 'a[href^="#"]', function(e) {
+    e.preventDefault();
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $($(this).attr("href")).offset().top
+        },
+        500,
+        "swing"
+      );
+  });
 });
